2d7d056bcd437d636734cf3db7e8356c901c41ee
Lars Gebraad <lars.gebraad@erdw.ethz.ch>
2022-01-28 16:15:03 +0100

    Merged animation on non-animation versions of SVGD

--- src/simpleSVGD/__init__.py
+++ src/simpleSVGD/__init__.py
@@ -2,6 +2,8 @@ import numpy as _np
 import tqdm as _tqdm
 from scipy.spatial.distance import pdist as _pdist, squareform as _squareform
 import matplotlib.pyplot as _plt
+import matplotlib.figure as _figure
+from typing import Callable as _Callable, List as _List, Tuple as _Tuple
 
 
 def _rbf_kernel(theta, h=-1):
@@ -23,98 +25,121 @@ def _rbf_kernel(theta, h=-1):
     return (Kxy, dxkxy)
 
 
-def update(x0, gradient_fn, n_iter=1000, stepsize=1e-3, bandwidth=-1, alpha=0.9, h=-1):
+def update(
+    x0: _np.array,
+    gradient_fn: _Callable,
+    # All following parameters are tuning parameters
+    n_iter: int = 1000,
+    stepsize: float = 1e-3,
+    bandwidth: float = -1,
+    alpha: float = 0.9,
+    # All following parameter only concern animation
+    animate: bool = False,
+    figure: _figure.Figure = None,
+    dimensions_to_plot: _List[float] = [0, 1],
+    background: _Tuple[_np.array] = None,
+):
+    """
+    Function to update a collection of samples using the SVGD algorithm.
+
+    x0
+        The initial samples are given to the function using x0, which has shape
+        (n_samples, function_dimensionality).
+
+    gradient_fn
+        A callable which computes the gradients of the function to approximate.
+        No values of the actual function are needed. Should accept multiple
+        points in input space (i.e. the shape of x0) and return a gradient of
+        the same shape as its input. In a Bayesian context; the actual
+        gradients to pass are those of the negative log likelihood.
+
+    n_iter
+        Number of iterations to do on the samples. Default is 1000.
+
+    stepsize
+        Stepsize of the SVGD algorithm. Default is 1e-3.
+
+    bandwidth
+        Bandwidth of the radial basis functions. Omitting this (or passing -1)
+        automatically computes it based on the distribution of samples. This is
+        more likely to produce good results, but will slow the algorithm down.
+        Default is -1.
+
+    alpha
+        Parameter with which to dampen gradient changes in the target function
+        during SVGD updating. Default is 0.9.
+
+    animate
+        A boolean to animate the algorithm. Only works for functions of at
+        least two dimensions. Default is False.
+
+    figure
+        A figure object in which to use the current axis for the animation.
+        Passing None will create a new figure. Default is None.
+
+    dimensions_to_plot
+        An array of two ints which describe which two dimensions to animate of
+        any higher dimensional function. Default is [0, 1].
+
+    background
+        A tuple of three arrays, used for the first three arguments of
+        matplotlib.pyplot.contour, to make plotting a background easier.
+    """
     # Check input
     if x0 is None or gradient_fn is None:
         raise ValueError("x0 or gradient_fn cannot be None!")
 
-    theta = _np.copy(x0)
+    if animate:
+        if figure is None:
+            figure = _plt.figure(figsize=(8, 8))
+        axis = _plt.gca()
 
-    # adagrad with momentum
-    fudge_factor = 1e-6
-    historical_grad = 0
-    try:
-        for iter in _tqdm.trange(n_iter):
-
-            lnpgrad = gradient_fn(theta)
-            # calculating the kernel matrix
-            kxy, dxkxy = _rbf_kernel(theta, h=h)
-            grad_theta = (_np.matmul(kxy, lnpgrad) + dxkxy) / x0.shape[0]
-
-            # adagrad
-            if iter == 0:
-                historical_grad = historical_grad + grad_theta ** 2
-            else:
-                historical_grad = alpha * historical_grad + (1 - alpha) * (
-                    grad_theta ** 2
-                )
-            adj_grad = _np.divide(grad_theta, fudge_factor + _np.sqrt(historical_grad))
-            theta = theta + stepsize * adj_grad
-    except KeyboardInterrupt:
-        pass
-
-    return theta
-
-
-def update_visual(
-    x0,
-    lnprob,
-    n_iter=1000,
-    stepsize=1e-3,
-    bandwidth=-1,
-    alpha=0.9,
-    figure=None,
-    dimensions_to_plot=[0, 1],
-    background=None,
-):
-
-    if figure is None:
-        figure = _plt.figure(figsize=(8, 8))
-
-    axis = _plt.gca()
-
-    # Check input
-    if x0 is None or lnprob is None:
-        raise ValueError("x0 or lnprob cannot be None!")
-
-    theta = _np.copy(x0)
+    # To make sure we don't accidentally update a variable we're not supposed
+    # to (i.e. avoid changing a variable by reference)
+    x0_updated = _np.copy(x0)
 
     # adagrad with momentum
     fudge_factor = 1e-6
     historical_grad = 0
 
-    if background is not None:
-        x1s, x2s, background_image = background
+    if animate:
+        if background is not None:
+            x1s, x2s, background_image = background
 
-        axis.contour(
-            x1s,
-            x2s,
-            _np.exp(-background_image),
-            levels=20,
-            alpha=0.5,
-            zorder=0,
+            axis.contour(
+                x1s,
+                x2s,
+                _np.exp(-background_image),
+                levels=20,
+                alpha=0.5,
+                zorder=0,
+            )
+
+        scatter = axis.scatter(
+            x0_updated[:, dimensions_to_plot[0]],
+            x0_updated[:, dimensions_to_plot[1]],
         )
 
-    scatter = axis.scatter(
-        theta[:, dimensions_to_plot[0]], theta[:, dimensions_to_plot[1]]
-    )
+        if background is not None:
+            _plt.xlim([x1s.min(), x1s.max()])
+            _plt.ylim([x2s.min(), x2s.max()])
 
-    if background is not None:
-        _plt.xlim([x1s.min(), x1s.max()])
-        _plt.ylim([x2s.min(), x2s.max()])
+        axis.set_aspect(1)
 
-    axis.set_aspect(1)
+        figure.canvas.draw()
+        _plt.pause(0.00001)
 
-    figure.canvas.draw()
-    _plt.pause(0.00001)
-
+    # The Try/Except allows on to interrupt the algorithm using CTRL+C while
+    # still getting x0_updated at the point of interruption.
     try:
         for iter in _tqdm.trange(n_iter):
 
-            lnpgrad = lnprob(theta)
+            # Calculate all the gradients of the -log(p)
+            grad_neglogp = -gradient_fn(x0_updated)
+
             # calculating the kernel matrix
-            kxy, dxkxy = _rbf_kernel(theta, h=-1)
-            grad_theta = (_np.matmul(kxy, lnpgrad) + dxkxy) / x0.shape[0]
+            kxy, dxkxy = _rbf_kernel(x0_updated, h=bandwidth)
+            grad_theta = (_np.matmul(kxy, grad_neglogp) + dxkxy) / x0.shape[0]
 
             # adagrad
             if iter == 0:
@@ -123,21 +148,36 @@ def update_visual(
                 historical_grad = alpha * historical_grad + (1 - alpha) * (
                     grad_theta ** 2
                 )
-            adj_grad = _np.divide(grad_theta, fudge_factor + _np.sqrt(historical_grad))
-            theta = theta + stepsize * adj_grad
+            adj_grad = _np.divide(
+                grad_theta, fudge_factor + _np.sqrt(historical_grad)
+            )
+            x0_updated = x0_updated + stepsize * adj_grad
 
-            scatter.set_offsets(
-                _np.hstack(
-                    (
-                        theta[:, dimensions_to_plot[0], None],
-                        theta[:, dimensions_to_plot[1], None],
+            if animate:
+                scatter.set_offsets(
+                    _np.hstack(
+                        (
+                            x0_updated[:, dimensions_to_plot[0], None],
+                            x0_updated[:, dimensions_to_plot[1], None],
+                        )
                     )
                 )
-            )
-            figure.canvas.draw()
-            _plt.pause(0.00001)
+                figure.canvas.draw()
+                _plt.pause(0.00001)
 
     except KeyboardInterrupt:
         pass
 
-    return theta
+    return x0_updated
+
+
+def gradient_vectorizer(non_vectorized_gradient: _Callable):
+    def grd(m: _np.array) -> _np.array:
+        return _np.hstack(
+            [
+                non_vectorized_gradient(m[idm, :, None])
+                for idm in range(m.shape[0])
+            ]
+        ).T
+
+    return grd
