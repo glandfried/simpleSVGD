4621c7d05453ebfd5274021e2b155af21bf82801
Lars Gebraad <lars.gebraad@erdw.ethz.ch>
2022-02-08 00:50:04 +0100

    Hid torch import

--- src/simpleSVGD/helpers.py
+++ src/simpleSVGD/helpers.py
@@ -1,12 +1,12 @@
-import torch
+import torch as _torch
 
 
 def TorchWrapper(g_fn, kernel):
-    class _internalClass(torch.autograd.Function):
+    class _internalClass(_torch.autograd.Function):
         @staticmethod
         def forward(ctx, input):
             ctx.save_for_backward(input)
-            return input.type(torch.FloatTensor)
+            return input.type(_torch.FloatTensor)
 
         @staticmethod
         def backward(ctx, grad_output):
@@ -14,13 +14,15 @@ def TorchWrapper(g_fn, kernel):
 
             kxy, dxkxy = kernel(input.numpy(), h=-1)
 
-            K = torch.from_numpy(kxy).type(torch.FloatTensor)
-            dk = torch.from_numpy(dxkxy).type(torch.FloatTensor)
+            K = _torch.from_numpy(kxy).type(_torch.FloatTensor)
+            dk = _torch.from_numpy(dxkxy).type(_torch.FloatTensor)
 
             return grad_output * (
                 K
-                @ torch.from_numpy(g_fn(input.numpy())).type(torch.FloatTensor)
+                @ _torch.from_numpy(g_fn(input.numpy())).type(
+                    _torch.FloatTensor
+                )
                 - dk
-            ).type(torch.FloatTensor)
+            ).type(_torch.FloatTensor)
 
     return _internalClass.apply
